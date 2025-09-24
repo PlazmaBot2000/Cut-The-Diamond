075d5c9c810f0c857dc66c19cb1d233a45b65973
PlazmaBot2000 <guardian9641@gmail.com>
2025-09-24 10:34:41 +0300

    Added v0.1.2

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import subprocess
+import os
 import json
+import bashlex
+
+MaxTask = 2
 
 
 
-#============================== Functions ===================================
+#============================== Run command function ===================================
 def run_command(command):
     try:
         result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
@@ -13,31 +17,80 @@ def run_command(command):
 
 
 
-#============================ Load JSON file ================================
-with open('tasks.json', 'r') as file:
-    tasks = json.load(file)
+#================================= Menu function =======================================
+def fail_menu(n):
+    print('Menu:')
+    print('    Try again (1)')
+    print('    Select another task (2)')
+    print('    Exit (3)')
+    Select = int(input('Select:'))
+    match Select:
+        case 1:
+            task(n)
+        case 2:
+            try:
+                task(int(input('Enter task number: ')))
+            except:
+                print('There isn\'t task associated with this number')
+        case 3:
+            exit
+
+def main_menu(n):
+    print('Menu:')
+    print('    Do the next task (1)')
+    print('    Select another task (2)')
+    print('    Exit (3)')
+    Select = int(input('Select:'))
+    match Select:
+        case 1:
+            try:
+                task(n+1)
+            except:
+                print('There isn\'t task associated with this number')
+        case 2:
+            try:
+                task(int(input('Enter task number: ')))
+            except:
+                print('There isn\'t task associated with this number')
+        case 3:
+            exit
 
 
 
-#============================= Parse JSON ===================================
-task_number = input('Enter task number: ')
-task_name = tasks[task_number]['name']
-task_start_cmd = tasks[task_number]['startcmd']
-task_result = tasks[task_number]['result']
-print('Selected task: ' + task_number + ' ' + task_name)
-print('Result: ' + task_result)
+
+
+#================================= Task function =======================================
+def task(n):
+    with open('tasks.json', 'r') as file:
+        tasks = json.load(file)
+
+    task_number = str(n)
+    task_name = tasks[task_number]['name']
+    task_start_cmd = tasks[task_number]['startcmd']
+    task_result = tasks[task_number]['result']
+    os.system('clear')
+    print('Selected task: ' + task_number + ' ' + task_name)
+    print('Result: ' + task_result)
+
+
+    command_input = task_start_cmd + input('Command: ' + task_start_cmd)
+    #print(bashlex.parse(command_input))
+    output = run_command(command_input).replace("\n", "")
+    print('Output: ' + output)
+
+
+    if output == task_result:
+        print('Congratulations! You solved task №' + task_number + ' ' + task_name)
+        main_menu(n)
+    else:
+        print('No')
+        fail_menu(n)
 
 
 
-#========================= Enter and run command ============================
-command_input = input('Command: ' + task_start_cmd)
-output = run_command(command_input).replace("\n", "")
-print('Output: ' + output)
 
 
 
-#============================= Check answer =================================
-if output == task_result:
-    print('Congratulations! You solved task №' + task_number + ' ' + task_name + '.')
-else:
-    print('No')
+
+#====================================== Main ===========================================
+main_menu(0)
